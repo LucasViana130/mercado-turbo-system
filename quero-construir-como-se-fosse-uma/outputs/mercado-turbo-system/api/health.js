6b566e938ced076ab9a022a755c301de1529278f
LucasViana <lucasgviana1@gmail.com>
2026-06-29 09:28:09 -0300

    Delete quero-construir-como-se-fosse-uma/outputs/mercado-turbo-system directory

--- quero-construir-como-se-fosse-uma/outputs/mercado-turbo-system/api/health.js
+++
@@ -1,8 +0,0 @@
-const { sendJson } = require("./_shared/http");
-
-module.exports = async function handler(req, res) {
-  sendJson(req, res, 200, {
-    ok: true,
-    service: "mercado-turbo-system"
-  });
-};
