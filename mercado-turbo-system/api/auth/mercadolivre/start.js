6cfcf1e91f7000d60852b693c4c098c469b08533
LucasViana <lucasgviana1@gmail.com>
2026-06-29 09:33:05 -0300

    Delete mercado-turbo-system directory

--- mercado-turbo-system/api/auth/mercadolivre/start.js
+++
@@ -1,9 +0,0 @@
-const { buildAuthUrl } = require("../../_shared/meli");
-
-module.exports = async function handler(req, res) {
-  try {
-    res.redirect(302, buildAuthUrl());
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
