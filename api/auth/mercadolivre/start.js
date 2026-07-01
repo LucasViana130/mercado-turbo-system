d9d7189bb9d60909e6d1d03bfeef31545e2ff0e4
LucasViana130 <lucasgviana1@gmail.com>
2026-07-01 11:49:04 -0300

    fix bug

--- api/auth/mercadolivre/start.js
+++ api/auth/mercadolivre/start.js
@@ -1,12 +1,9 @@
 const { buildAuthUrl } = require("../../_shared/meli");
 
 module.exports = async function handler(req, res) {
-  try {
-    res.redirect(302, buildAuthUrl());
-  } catch (error) {
-    res.status(500).send(`
-      <h1>Erro</h1>
-      <pre>${error.stack}</pre>
-    `);
-  }
+  res.json({
+    clientId: process.env.MELI_CLIENT_ID,
+    redirectUri: process.env.MELI_REDIRECT_URI,
+    authUrl: buildAuthUrl()
+  });
 };
