2b0b67480b0a235b1a0ea09a837f2f5052684f40
LucasViana <lucasgviana1@gmail.com>
2026-07-01 11:34:58 -0300

    Improve error response format in auth handler
    Updated error handling to send a formatted HTML response with error stack.

--- api/auth/mercadolivre/start.js
+++ api/auth/mercadolivre/start.js
@@ -4,6 +4,9 @@ module.exports = async function handler(req, res) {
   try {
     res.redirect(302, buildAuthUrl());
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(`
+      <h1>Erro</h1>
+      <pre>${error.stack}</pre>
+    `);
   }
 };
